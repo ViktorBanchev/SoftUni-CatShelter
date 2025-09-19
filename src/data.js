14d2010dc255c40a50b81508591682648673aa6d
Vikicha089 <vbanchevf@gmail.com>
2025-09-20 02:22:11 +0300

    Lecture finish

--- src/data.js
+++ src/data.js
@@ -7,11 +7,26 @@ export async function getCats() {
     return db.cats;
 }
 
+export async function getCat(id) {
+    return db.cats.find(cat => cat.id === id);
+}
 
-export async function saveCat(cat) {
-    db.cats.push(cat);
+export async function saveCat(catData) {
+    db.cats.push({
+        id: db.cats[db.cats.length - 1].id + 1,
+        ...catData
+    })
+    await saveDb();
 
+}
+
+export async function updateCat(catId, catData) {
+    db.cats = db.cats.map(cat => cat.id === catId ? { id: catId, ...catData } : cat);
+    await saveDb();
+}
+
+async function saveDb() {
     const dbSerialized = JSON.stringify(db, null, 2);
 
-    await fs.writeFile("./src/db.json", dbSerialized, {encoding: "utf-8"});
+    await fs.writeFile("./src/db.json", dbSerialized, { encoding: "utf-8" });
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import http from "http";
 import fs from "fs/promises";
 
-import {getCats, saveCat} from "./data.js";
+import { getCat, getCats, saveCat, updateCat } from "./data.js";
 
 const server = http.createServer(async (req, res) => {
     let html;
@@ -10,17 +10,21 @@ const server = http.createServer(async (req, res) => {
         console.log("POST Requst has been made");
 
         let data = '';
-
         req.on('data', (chunk) => {
             data += chunk.toString();
         });
 
         req.on('end', async () => {
             const searchParams = new URLSearchParams(data);
+            const catData = Object.fromEntries(searchParams.entries());
 
-            const newCat = Object.fromEntries(searchParams.entries());
+            if (req.url === "/cats/add-cat") {
+                await saveCat(catData)
+            } else if (req.url.startsWith("/cats/edit-cat")) {
+                const catId = getCatId(req.url);
+                await updateCat(catId, catData);
+            }
 
-            await saveCat(newCat)
 
             res.writeHead(302, {
                 'location': '/'
@@ -31,29 +35,36 @@ const server = http.createServer(async (req, res) => {
         return
     }
 
+    if (req.url.startsWith('/cats/edit-cat')) {
+        const catId = getCatId(req.url)
+        const cat = await getCat(catId);
+        html = await editCatView(cat);
 
-    switch (req.url) {
-        case "/":
-            html = await homeView()
-            break;
-        case "/styles/site.css":
-            const homeCss = await fs.readFile('./src/styles/site.css', { encoding: 'utf-8' });
 
-            res.writeHead(200, {
-                "content-type": "text/css",
-                "cache-control": "max-age=10"
-            })
+    } else {
+        switch (req.url) {
+            case "/":
+                html = await homeView()
+                break;
+            case "/styles/site.css":
+                const homeCss = await fs.readFile('./src/styles/site.css', { encoding: 'utf-8' });
 
-            res.write(homeCss);
-            return res.end();
-        case "/cats/add-breed":
-            html = await addBreedView();
-            break;
-        case "/cats/add-cat":
-            html = await addCatView();
-            break;
-        default:
-            return res.end();
+                res.writeHead(200, {
+                    "content-type": "text/css",
+                    "cache-control": "max-age=10"
+                })
+
+                res.write(homeCss);
+                return res.end();
+            case "/cats/add-breed":
+                html = await addBreedView();
+                break;
+            case "/cats/add-cat":
+                html = await addCatView();
+                break;
+            default:
+                return res.end();
+        }
     }
 
     res.writeHead(200, {
@@ -76,7 +87,7 @@ function catTemplate(cat) {
             <p><span>Breed: </span>${cat.breed}</p>
             <p><span>Description: </span>${cat.description}</p>
             <ul class="buttons">
-                <li class="btn edit"><a href="">Change Info</a></li>
+                <li class="btn edit"><a href="/cats/edit-cat/${cat.id}/">Change Info</a></li>
                 <li class="btn delete"><a href="">New Home</a></li>
             </ul>
         </li>
@@ -89,7 +100,7 @@ async function homeView() {
     const cats = await getCats();
     const catsHtml = cats.map(cat => catTemplate(cat)).join('\n')
     const result = html.replaceAll('{{cats}}', catsHtml)
-    
+
     return result;
 }
 
@@ -101,4 +112,21 @@ async function addBreedView() {
 async function addCatView() {
     const html = await fs.readFile('./src/views/addCat.html', { encoding: "utf-8" });
     return html
+}
+
+async function editCatView(cat) {
+    let html = await fs.readFile('./src/views/editCat.html', { encoding: "utf-8" });
+    const entries = Object.entries(cat);
+    entries.forEach(entry => {
+        let testVar = `{{${entry[0]}}}`
+        html = html.replaceAll(testVar, entry[1])
+    })
+
+    return html;
+}
+
+function getCatId(reqUrl) {
+    const segments = reqUrl.split("/");
+    const catId = Number(segments[3]);
+    return catId;
 }
